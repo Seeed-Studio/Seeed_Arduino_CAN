5c09068de68528e071b698b6b0cb71c82e23833f
Dmitry <dmitry@pereslegin.ru>
2016-01-13 15:18:58 +0300

    Whitespaces

--- examples/CAN_read/CAN_read.ino
+++ examples/CAN_read/CAN_read.ino
@@ -10,24 +10,22 @@ void setup() {
   mcp2551.begin(CAN_125KBPS);
   Serial.println("------- CAN Read ----------");
   Serial.println("ID  DLC   DATA");
-  
 }
 
 void loop() {
   
   if (mcp2551.readMessage(&canMsg) == MCP_CAN::ERROR_OK) {
-        
+      
     Serial.print(canMsg.can_id); // print ID
 
     Serial.print(canMsg.can_dlc); // print DLC
 
-    for(int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
         
-        Serial.print(canMsg.data[i]);
-        Serial.print(" ");
+      Serial.print(canMsg.data[i]);
+      Serial.print(" ");
 
     }
-
 
     Serial.println();      
   }
