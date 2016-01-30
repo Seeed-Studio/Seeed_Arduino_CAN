1465f5d6195468336d72f7ab0d87ad4bfffb7e11
Jan Kirchner <jkirchner@deutsch.cz>
2016-01-30 23:39:52 +0100

    Goto + label removing. Standard loop is a cleaner solution.

--- examples/send/send.ino
+++ examples/send/send.ino
@@ -12,19 +12,13 @@ void setup()
 {
     Serial.begin(115200);
 
-START_INIT:
-
-    if(CAN_OK == CAN.begin(CAN_500KBPS))                   // init can bus : baudrate = 500k
-    {
-        Serial.println("CAN BUS Shield init ok!");
-    }
-    else
+    while (CAN_OK != CAN.begin(CAN_500KBPS))              // init can bus : baudrate = 500k
     {
         Serial.println("CAN BUS Shield init fail");
-        Serial.println("Init CAN BUS Shield again");
+        Serial.println(" Init CAN BUS Shield again");
         delay(100);
-        goto START_INIT;
     }
+    Serial.println("CAN BUS Shield init ok!");
 }
 
 unsigned char stmp[8] = {0, 1, 2, 3, 4, 5, 6, 7};
