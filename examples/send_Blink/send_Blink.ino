4050a95ba5392c9f284b47ce1b8d5a96b7fef1df
Jared <jared@quantumcodeworks.com>
2016-07-05 17:05:01 -0400

    Update send_Blink.ino
    Corrected some comments while reading through this

--- examples/send_Blink/send_Blink.ino
+++ examples/send_Blink/send_Blink.ino
@@ -27,9 +27,9 @@ unsigned char stmp[8] = {ledHIGH, 1, 2, 3, ledLOW, 5, 6, 7};
 
 void loop()
 {   Serial.println("In loop");
-    // send data:  id = 0x00, standrad frame, data len = 8, stmp: data buf
+    // send data:  id = 0x00, standard frame, data len = 8, stmp: data buf
     CAN.sendMsgBuf(0x70,0, 8, stmp);
-    delay(1000);                       // send data per 100ms
+    delay(1000);                       // send data once per second
 }
 
 /*********************************************************************************************************
