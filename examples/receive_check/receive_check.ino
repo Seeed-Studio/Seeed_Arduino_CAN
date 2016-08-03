190d33bd908ae514c47e68f14eb3458a04b002ae
Lowaichung <luweicong@seeedstudio.com>
2016-08-03 09:45:43 +0800

    Update receive_check.ino

--- examples/receive_check/receive_check.ino
+++ examples/receive_check/receive_check.ino
@@ -36,7 +36,7 @@ void loop()
     {
         CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
 
-        unsigned char canId = CAN.getCanId();
+        unsigned int canId = CAN.getCanId();
         
         Serial.println("-----------------------------");
         Serial.print("Get data from ID: ");
