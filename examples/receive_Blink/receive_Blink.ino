51511d5cfa869cdd4f091c86b4f0387202272d96
wangcoolc <1387172050@163.com>
2020-12-07 17:29:13 +0800

    Complete the receive blink function

--- examples/receive_Blink/receive_Blink.ino
+++ examples/receive_Blink/receive_Blink.ino
@@ -3,7 +3,7 @@
 // loovee, 2014-6-13
 
 #include <SPI.h>
-#include "mcp_can.h"
+#include "mcp2518fd_can.h"
 
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
@@ -14,17 +14,20 @@
 
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
-const int SPI_CS_PIN = 9;
-const int LED        = 8;
+const int SPI_CS_PIN = BCM8;
+mcp2518fd* controller;
+
+const int LED        = LED_BUILTIN;
 boolean ledON        = 1;
-
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
-
+                                  // Set CS pin
 void setup() {
     SERIAL.begin(115200);
+    while(!Serial){};
+    controller = new mcp2518fd();
+    controller->mcp_canbus(SPI_CS_PIN);
     pinMode(LED, OUTPUT);
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+    while (CAN_OK != controller->begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
         SERIAL.println("CAN BUS Shield init fail");
         SERIAL.println("Init CAN BUS Shield again");
         delay(100);
@@ -37,10 +40,10 @@ void loop() {
     unsigned char len = 0;
     unsigned char buf[8];
 
-    if (CAN_MSGAVAIL == CAN.checkReceive()) {         // check if data coming
-        CAN.readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
+    if (1 == controller->checkReceive()) {         // check if data coming
+        controller->readMsgBuf(&len, buf);    // read data,  len: data length, buf: data buf
 
-        unsigned long canId = CAN.getCanId();
+        unsigned long canId = controller->getCanId();
 
         SERIAL.println("-----------------------------");
         SERIAL.println("get data from ID: 0x");
