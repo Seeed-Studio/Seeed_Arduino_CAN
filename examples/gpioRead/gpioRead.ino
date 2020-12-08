eb1dd3b2c4ec15f2827f6d8f4bc8d36fc2e7b78b
wangcoolc <1387172050@163.com>
2020-12-08 18:01:52 +0800

    Complete canbus gpio read example

--- examples/gpioRead/gpioRead.ino
+++ examples/gpioRead/gpioRead.ino
@@ -1,7 +1,11 @@
 // demo: Use TX2RTS as digital input
 // adlerweb, 2017-06-24
 #include <SPI.h>
-#include "mcp_can.h"
+#include "mcp2515_can.h"
+#include "mcp2518fd_can.h"
+
+#define CAN_2518FD
+#define SPI_CS_PIN BCM8
 
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
@@ -10,21 +14,32 @@
     #define SERIAL Serial
 #endif
 
-#define SPI_CS_PIN 10
+#ifdef CAN_2518FD
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
 
-MCP_CAN CAN(SPI_CS_PIN); // Set CS pin
-
+#ifdef CAN_2515
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif
 
 void setup() {
     SERIAL.begin(115200);
-
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+#ifdef CAN_2518FD
+    while (0 != CAN.begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
+#endif
+#ifdef CAN_2515
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {
+#endif
+   
         SERIAL.println("CAN init failed, retry");
         delay(100);
     }
     SERIAL.println("CAN init ok");
-
+#ifdef CAN_2518FD
+    if (CAN.mcpPinMode(GPIO_PIN_0, GPIO_MODE_INT)) {
+#else
     if (CAN.mcpPinMode(MCP_TX2RTS, MCP_PIN_IN)) {
+#endif
         SERIAL.println("TX2RTS is now an input");
     } else {
         SERIAL.println("Could not switch TX2RTS");
@@ -33,7 +48,11 @@ void setup() {
 
 void loop() {
     SERIAL.print("TX2RTS is currently ");
+#ifdef CAN_2518FD
+    SERIAL.println(CAN.mcpDigitalRead(GPIO_PIN_0));
+#else
     SERIAL.println(CAN.mcpDigitalRead(MCP_TX2RTS));
+#endif
     delay(500);
 }
 
