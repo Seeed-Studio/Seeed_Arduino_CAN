6e9112c4f11bc2ce9dce676ba7c98d11524b629b
wangcoolc <1387172050@163.com>
2020-12-08 19:58:47 +0800

    Complete can bus receive interrupt example

--- examples/receive_interrupt/receive_interrupt.ino
+++ examples/receive_interrupt/receive_interrupt.ino
@@ -3,7 +3,8 @@
 // loovee, 2014-6-13
 
 #include <SPI.h>
-#include "mcp_can.h"
+#include "mcp2515_can.h"
+#include "mcp2518fd_can.h"
 
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
@@ -12,12 +13,20 @@
     #define SERIAL Serial
 #endif
 
+#define CAN_2518FD
+
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
-const int SPI_CS_PIN = 9;
-const int CAN_INT_PIN = 2;
+const int SPI_CS_PIN = BCM8;
+const int CAN_INT_PIN = BCM25;
 
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+#ifdef CAN_2518FD
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+
+#ifdef CAN_2515
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif                              // Set CS pin
 
 
 unsigned char flagRecv = 0;
@@ -30,13 +39,17 @@ void setup() {
     while (!SERIAL) {
         ; // wait for serial port to connect. Needed for native USB port only
     }
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+#ifdef CAN_2518FD
+    while (0 != CAN.begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
+#else
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+#endif            // init can bus : baudrate = 500k
         SERIAL.println("CAN BUS Shield init fail");
         SERIAL.println(" Init CAN BUS Shield again");
         delay(100);
     }
     SERIAL.println("CAN BUS Shield init ok!");
-
+    pinMode(CAN_INT_PIN, INPUT);
     attachInterrupt(digitalPinToInterrupt(CAN_INT_PIN), MCP2515_ISR, FALLING); // start interrupt
 }
 
@@ -49,13 +62,14 @@ void loop() {
         // check if get data
 
         flagRecv = 0;                   // clear flag
-
+        SERIAL.println("into loop");
         // iterate over all pending messages
         // If either the bus is saturated or the MCU is busy,
         // both RX buffers may be in use and reading a single
         // message does not clear the IRQ conditon.
         while (CAN_MSGAVAIL == CAN.checkReceive()) {
             // read data,  len: data length, buf: data buf
+            SERIAL.println("checkReceive");
             CAN.readMsgBuf(&len, buf);
 
             // print the data
