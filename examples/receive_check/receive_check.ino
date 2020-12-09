68da38c24b35edc99b8e79e4abcd1c548bb02c16
wangcoolc <1387172050@163.com>
2020-12-09 16:37:06 +0800

    Modified canbus receive check example

--- examples/receive_check/receive_check.ino
+++ examples/receive_check/receive_check.ino
@@ -4,8 +4,6 @@
 
 
 #include <SPI.h>
-#include "mcp_can.h"
-
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
     #define SERIAL SerialUSB
@@ -13,16 +11,33 @@
     #define SERIAL Serial
 #endif
 
+//#define CAN_2515
+#define CAN_2518FD
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
-const int SPI_CS_PIN = 9;
 
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+const int SPI_CS_PIN = BCM8;
+const int CAN_INT_PIN = BCM25;
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+
+#ifdef CAN_2515
+#include "mcp2515_can.h"
+const int SPI_CS_PIN = 9;
+const int CAN_INT_PIN = 2;
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif                           
 
 void setup() {
     SERIAL.begin(115200);
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+#ifdef CAN_2518FD
+    while (0 != CAN.begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
+#else
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+#endif
         SERIAL.println("CAN BUS Shield init fail");
         SERIAL.println(" Init CAN BUS Shield again");
         delay(100);
