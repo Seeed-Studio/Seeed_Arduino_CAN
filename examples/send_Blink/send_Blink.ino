8c05f0b016da36c6df53ddacfd513c193a2297e5
wangcoolc <1387172050@163.com>
2020-12-09 16:42:47 +0800

    Modified canbus send Blink example

--- examples/send_Blink/send_Blink.ino
+++ examples/send_Blink/send_Blink.ino
@@ -1,5 +1,4 @@
 // demo: CAN-BUS Shield, send data
-#include <mcp_can.h>
 #include <SPI.h>
 
 /*SAMD core*/
@@ -9,18 +8,37 @@
     #define SERIAL Serial
 #endif
 
+//#define CAN_2515
+#define CAN_2518FD
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
+
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+const int SPI_CS_PIN = BCM8;
+const int CAN_INT_PIN = BCM25;
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+
+#ifdef CAN_2515
+#include "mcp2515_can.h"
 const int SPI_CS_PIN = 9;
+const int CAN_INT_PIN = 2;
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif                              // Set CS pin
+
 const int ledHIGH    = 1;
 const int ledLOW     = 0;
-
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
+                           
 
 void setup() {
     SERIAL.begin(115200);
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+#ifdef CAN_2518FD
+    while (0 != CAN.begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
+#else
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+#endif
         SERIAL.println("CAN BUS Shield init fail");
         SERIAL.println(" Init CAN BUS Shield again");
         delay(100);
