ac609825b204cc44a1cc04fe276d5ecc899cf4e1
wangcoolc <1387172050@163.com>
2020-12-09 16:31:13 +0800

    Modified canbus receive blink example

--- examples/receive_Blink/receive_Blink.ino
+++ examples/receive_Blink/receive_Blink.ino
@@ -3,7 +3,6 @@
 // loovee, 2014-6-13
 
 #include <SPI.h>
-#include "mcp_can.h"
 
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
@@ -12,19 +11,37 @@
     #define SERIAL Serial
 #endif
 
+#define CAN_2515
+//#define CAN_2518FD
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
+
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+const int SPI_CS_PIN = BCM8;
+const int CAN_INT_PIN = BCM25;
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+
+#ifdef CAN_2515
+#include "mcp2515_can.h"
 const int SPI_CS_PIN = 9;
+const int CAN_INT_PIN = 2;
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif                              // Set CS pin
+
 const int LED        = 8;
 boolean ledON        = 1;
-
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
 
 void setup() {
     SERIAL.begin(115200);
     pinMode(LED, OUTPUT);
 
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+#ifdef CAN_2518FD
+    while (0 != CAN.begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
+#else
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+#endif 
         SERIAL.println("CAN BUS Shield init fail");
         SERIAL.println("Init CAN BUS Shield again");
         delay(100);
