e2d0f87918144ae293ce0e8b891b0bd81e3deb2e
wangcoolc <1387172050@163.com>
2020-12-09 11:21:24 +0800

    Complete canbus set_mask_filter_recv example

--- examples/set_mask_filter_recv/set_mask_filter_recv.ino
+++ examples/set_mask_filter_recv/set_mask_filter_recv.ino
@@ -4,7 +4,6 @@
 // loovee, 2014-7-8
 
 #include <SPI.h>
-#include "mcp_can.h"
 
 /*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
@@ -13,13 +12,24 @@
     #define SERIAL Serial
 #endif
 
+//#define CAN_2518FD
+#define CAN_2515
 // the cs pin of the version after v1.1 is default to D9
 // v0.9b and v1.0 is default D10
+
+#ifdef CAN_2518FD
+#include "mcp2518fd_can.h"
+const int SPI_CS_PIN = BCM8;
+mcp2518fd CAN(SPI_CS_PIN); // Set CS pin
+#endif
+
+#ifdef CAN_2515
+#include "mcp2515_can.h"
 const int SPI_CS_PIN = 9;
+mcp2515_can CAN(SPI_CS_PIN); // Set CS pin
+#endif                              
 
-MCP_CAN CAN(SPI_CS_PIN);                                    // Set CS pin
-
-const byte interruptPin = 0;
+const byte interruptPin = 2;
 unsigned char flagRecv = 0;
 unsigned char len = 0;
 unsigned char buf[8];
@@ -27,8 +37,13 @@ char str[20];
 
 void setup() {
     SERIAL.begin(115200);
-
-    while (CAN_OK != CAN.begin(CAN_500KBPS)) {            // init can bus : baudrate = 500k
+    while(!Serial){};
+    
+#ifdef CAN_2518FD
+    while (0 != CAN.begin((byte)CAN_500K_1M)) {            // init can bus : baudrate = 500k
+#else
+    while (CAN_OK != CAN.begin(CAN_500KBPS)) {             // init can bus : baudrate = 500k
+#endif          
         SERIAL.println("CAN BUS Shield init fail");
         SERIAL.println(" Init CAN BUS Shield again");
         delay(100);
