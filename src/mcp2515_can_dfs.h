ddf35e49357d68cdad686bf3ee9067e4d0668c7f
Baozhu Zuo <zuobaozhu@gmail.com>
2020-12-10 14:44:36 +0800

    use enum to aline bittime

--- src/mcp2515_can_dfs.h
+++ src/mcp2515_can_dfs.h
@@ -447,24 +447,28 @@
 #define CANDEFAULTIDENT     (0x55CC)
 #define CANDEFAULTIDENTEXT  (CAN_EXTID)
 
-#define CAN_5KBPS           1
-#define CAN_10KBPS          2
-#define CAN_20KBPS          3
-#define CAN_25KBPS          4
-#define CAN_31K25BPS        5
-#define CAN_33KBPS          6
-#define CAN_40KBPS          7
-#define CAN_50KBPS          8
-#define CAN_80KBPS          9
-#define CAN_83K3BPS         10
-#define CAN_95KBPS          11
-#define CAN_100KBPS         12
-#define CAN_125KBPS         13
-#define CAN_200KBPS         14
-#define CAN_250KBPS         15
-#define CAN_500KBPS         16
-#define CAN_666KBPS         17
-#define CAN_1000KBPS        18
+
+typedef enum {
+    CAN_NOBPS,
+    CAN_5KBPS,
+    CAN_10KBPS,
+    CAN_20KBPS,
+    CAN_25KBPS,
+    CAN_31K25BPS,
+    CAN_33KBPS  ,
+    CAN_40KBPS  ,
+    CAN_50KBPS  ,
+    CAN_80KBPS  ,
+    CAN_83K3BPS ,
+    CAN_95KBPS  ,
+    CAN_100KBPS ,
+    CAN_125KBPS ,
+    CAN_200KBPS ,
+    CAN_250KBPS ,
+    CAN_500KBPS ,
+    CAN_666KBPS ,
+    CAN_1000KBPS
+} MCP2515_BITTIME_SETUP;
 
 #define CAN_OK              (0)
 #define CAN_FAILINIT        (1)
